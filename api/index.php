25508f3562c79b215ba4b1777b147796839f121c
sarahamda2005@gmail.com <sarahamda2005@gmail.com>
2026-04-03 10:05:50 +0100

    update

--- api/index.php
+++ api/index.php
@@ -3,6 +3,6 @@ echo"<h1>Bonjour depuis PHP sur vercel </h1>";
 echo"<p>Deploiement reissi.</p>";
 echo"<a href='exercice1.php'>Exercice1</a>";
 echo"<a href='exercice2.php'>Exercice2</a>";
-echo"<a href=''>Rapport1</a>";
-echo"<a href=''>Rapport2</a>";
+echo"<a href='#'>Rapport1</a>";
+echo"<a href='#'>Rapport2</a>";
 ?>

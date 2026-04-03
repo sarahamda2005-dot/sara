facf0c7b3c103bf5e52b600cef10abecd74c2fff
sarahamda2005@gmail.com <sarahamda2005@gmail.com>
2026-04-03 11:43:51 +0100

    main

--- api/index.php
+++ api/index.php
@@ -1,8 +1,8 @@
 <?php
-echo"<h1>Bonjour depuis PHP sur vercel </h1>";
+echo"<h1>Bonjour  </h1>";
 echo"<p>Deploiement reissi.</p>";
 echo"<a href='exercice1.php'>Exercice1</a>";
 echo"<a href='exercice2.php'>Exercice2</a>";
-echo"<a href='#'>Rapport1</a>";
-echo"<a href='#'>Rapport2</a>";
+echo"<a href='Rapport  2.pdf'>Rapport1</a>";
+echo"<a href='Rapport  2.pdf'>Rapport2</a>";
 ?>
